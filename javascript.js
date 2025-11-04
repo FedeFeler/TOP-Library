ff32383cb4f8c1ea098b10950a7b27c9f1e7e584
Federico <fledesmarubulotta@gmail.com>
2025-11-04 17:29:00 -0300

    feat: add logic for adding books into the html

--- javascript.js
+++ javascript.js
@@ -15,3 +15,14 @@ function addBookToLibrary(name, author, pages) {
   const book = new Book(name, author, pages);
   myLibrary.push(book);
 }
+
+const container = document.querySelector(".container")
+
+for (let book of myLibrary) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.textContent = `Book Name: ${book.name}
+  Author: ${book.author}
+  Pages: ${book.pages}`;
+  container.appendChild(card);
+}
